72688b03d4a16caf08b746e2e0c9525ac7ff0a47
Dimitris Krestos <dkrestos@gmail.com>
2026-05-03 14:32:12 +0300

    Unify file upload limit labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def file_transfer_size_limit_label
+    bytes = Nullroom::Config::FILE_TRANSFER_SIZE_LIMIT_BYTES
+    mebibytes = bytes / 1024.0 / 1024.0
+
+    mebibytes.round == mebibytes ? "#{mebibytes.to_i} MB" : "#{mebibytes.round(1)} MB"
+  end
 end
